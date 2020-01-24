b0370d85050a76de7317fd335939745060ecd243
nicholsonja <nicholsonja@apsu.edu>
2020-01-24 12:44:08 -0600

    Update MainActivity.java

--- app/src/main/java/edu/apsu/csci/gittest/MainActivity.java
+++ app/src/main/java/edu/apsu/csci/gittest/MainActivity.java
@@ -8,10 +8,11 @@ import android.util.Log;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i("Test", "This is a test");
         Log.i("Test", "This is another test");
+        Log.i("Test", "This is third test");
     }
 }
